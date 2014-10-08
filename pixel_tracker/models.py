8de770ce095a19b5720b72809f4e40a065fd60aa
Dan Gayle <dang@spokesman.com>
2014-10-07 19:02:46 -0700

    Switched to decorator style receiver connection

--- pixel_tracker/models.py
+++ pixel_tracker/models.py
@@ -1,10 +1,12 @@
 from pprint import pprint
+from django.dispatch import receiver
 from .signals import pixel_data
+from .tasks import pretty_print_tracking_data
 
 
-def simple_receiver(**kwargs):
+@receiver(pixel_data)
+def example_receiver(**kwargs):
     """Example receiver for pixel_data."""
-    pixel_data = kwargs['pixel_data']
-    pprint(kwargs)
 
-pixel_data.connect(simple_receiver)
+    pixel_data = kwargs['pixel_data']
+    pprint(pixel_data)
